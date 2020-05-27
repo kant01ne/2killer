aa426bf1eb8960abb27cb0e37935f8d2d0adb34b
Bachir Çaoui <bachir@heetch.com>
2020-05-28 01:10:43 +0200

    Add algorithm explanation

--- app/Models/Assignment.js
+++ app/Models/Assignment.js
@@ -11,16 +11,64 @@ class Assignment {
     this.assignK(this.players[0])
   }
 
+  // Given 4 players [A, B, C, D] with kills [KA, KB, KC, KD]. We want to assign a killer and a
+  // victim (with a kill) to each one :
+  // - A player can't be oneself's killer
+  // - A player can't be oneself's victim
+  // - A player can't be assigned his own kill
+  // - A player can't be the victim of his own kill
+  // - The killer->victim graph between all players can't have circual sub-graphs. Ex: A kills B, B
+  // kills C, C kills A. This puts D outside the loop, with no one to kill but himself.
+  // 
+  // How this recursive algorithm works:
+  // - If there are no kills left in the pool, we stop the assignment
+  // - Shuffle the players [B, C, A, D]
+  //
+  // === First call === kills = [KA, KB, KC, KD]
+  // - We start with the first player B
+  // - Select a victim that is not the killer, that hasn't been assigned to a killer yet and is not
+  // part of the killer's grand killers list. Ex. : D.
+  // - If possible, select a kill that is neither the killer's or the victim's. Ex: KC
+  // - Update the killer B(assignedKill: KC, victim: D) and the victim D(killer: B, grandKillers: [B])
+  // - Remove KC from the kills list. list is now [KA, KB, KD]
+  // - Call this function again with the previous victim D as the killer
+  //
+  // Graph:
+  // B -> KC -> D
+  //
+  // === Second call === kills = [KA, KB, KD]
+  // - Killer : D(killer: B, grandKillers: [B])
+  // - Victim : A (can't be B as he is part of D's grandKillers. This avoids circular subgraphs).
+  // - Kill : KB (can't be KA or KD)
+  // - Update the killer D(victim: A, assignedKill: KB, killer: B, grandKillers: [B])
+  // - Update the victim A(killer: D, grandKillers: [B, D])
+  //
+  // Graph:
+  // B -> KC -> D -> KB -> A
+  // 
+  // === 3rd call === killls = [KA, KD]
+  // Killer A, Victim C, Kill KD
+  // Graph:
+  // B -> KC -> D -> KB -> A -> KD -> C
+  //
+  // === 4th call === kills = [KA]
+  // - The victim is necessarily the first player (hence the this.kills.length === 1
+  // check, returning the first element of this.players)
+  // Killer C, Victim B, Kill KA
+  // Graph:
+  // B -> KC -> D -> KB -> A -> KD -> C -> KA -> B
+  // 
+  // === 5th call === kills = []
+  // END
+  //
+  // Edge cases are detailed below.
   assignK(killer) {
     if (this.kills.length === 0) {
       return this.players
     }
 
-    // If 1 kill left, it means one victim is left, which is also the first person to have been
-    // assigned a victim (meaning it is the first grandkiller).
     var victim = _.find(this.players, (v) => (this.kills.length === 1) || (v.name !== killer.name && !v.has_a_killer() && !_.includes(killer.grandKillers, v)))
 
-    // Select a kill that is not the killer's or the victim's own kills.
     var kill = _.find(this.kills, (k) => k != killer.ownKill && k != victim.ownKill)
     if (kill === undefined) kill = this.kills[0]
 
@@ -57,7 +105,6 @@ class Assignment {
       console.log(killer.killer.toString())
     }
 
-    // Add the killer and all the killer's grandkillers to the killer's victim grand killers
     victim.grandKillers = [...victim.grandKillers, ...killer.grandKillers, killer]
     victim.killer = killer
     killer.assignedKill = kill
@@ -65,7 +112,6 @@ class Assignment {
     this.kills = _.without(this.kills, kill)
     console.log(killer.toString())
 
-    // Assign a victim to the killer's victim
     this.assignK(killer.victim)
   }
 }
